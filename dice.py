830eb8d8905f6badd10b150964eca4a694cc7b75
JackBui00 <jackbui05@hotmail.com>
2021-12-23 19:24:36 -0800

    comments, remove spacing in user input

--- dice.py
+++ dice.py
@@ -2,7 +2,14 @@ import random
 import re
 
 
-
+#Take in User Input 
 userInput= input("Input a dice\n")
+
+#Remove Spaceing in User Input 
 noSpaceInput = userInput.replace(' ','')
 print(noSpaceInput)
+
+
+
+
+
