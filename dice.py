287f44326fd65c739bf25475b509cf4304d1ac1b
JackBui00 <jackbui05@hotmail.com>
2021-12-23 20:06:34 -0800

    filter out integer values of both negative and positive to add or subtract from the final total

--- dice.py
+++ dice.py
@@ -4,7 +4,7 @@ import re
 
 #Take in User Input 
 #userInput= input("Input a dice\n")
-userInput= "1d3+1d4 +1d3 -1d3 +1d3"
+userInput= "1d3+1d4 +1d3+ 33 -1d3 +1d3-  5d5 -5 - 10 + 25"
 
 
 #Remove Spaceing in User Input 
@@ -26,15 +26,19 @@ print(negativeNumberMatch)
 
 positiveUserInputArray = [x for x in positiveUserInputArray if "-" not in x]
 
-#Filter Negative Attachment Match
+#Filter Negative Attachment Match, random characters
 positiveDiceMatch = [s for s in positiveUserInputArray if s.__contains__("d")]
+print("fiter negative attachment match")
 print(positiveDiceMatch)
 
+#Positive Integer Values from filter
 PositiveNumberMatch= []
 print(positiveUserInputArray)
 
+#Array for Dice Subtraction
 subtractionTotal = []
 
+#Split Apart Array Item 0 to check for dice stuck together
 for i in range(0,len(negativeNumberMatch)):
     splitNegativeNumberMatch = negativeNumberMatch[i].split('-')
     if "d" in str(splitNegativeNumberMatch[0]):
@@ -53,7 +57,23 @@ print(positiveDiceMatch)
 print("negative subtraction array")
 print(subtractionTotal)
 
+#integers to subtract 
+finalFilterNegativeValue = []
 
+negativeDiceMatch= [s for s in subtractionTotal if s.__contains__("d")]
 
+#Filter Integer Values in Array to Subtract from Final Dice Total 
+for items in subtractionTotal:
+    for subitem in items.split():
+        if(subitem.isdigit()):
+            finalFilterNegativeValue.append(subitem)
 
+#Filter Integer Values in Array to Add To Final Dice Total 
+for items in positiveUserInputArray:
+    for subitem in items.split():
+        if(subitem.isdigit()):
+            PositiveNumberMatch.append(subitem)
 
+print(splitNegativeNumberMatch)
+print(positiveDiceMatch)
+print(finalFilterNegativeValue)
